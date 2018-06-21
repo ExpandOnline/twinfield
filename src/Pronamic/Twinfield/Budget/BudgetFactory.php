03148b427c82fdff4656bb232f7e93bc2c3518cf
swordbeta <mvantricht@expandonline.nl>
2018-06-21 12:54:48 +0200

    S130.20 Also save budget name.
    Which happens to always be "Budget".

--- src/Pronamic/Twinfield/Budget/BudgetFactory.php
+++ src/Pronamic/Twinfield/Budget/BudgetFactory.php
@@ -27,7 +27,8 @@ class BudgetFactory extends FinderFactory
 		$budgets = [];
 		foreach($response->data->Items->ArrayOfString as $budgetArray)
 		{
-			$budgets[] = !is_array($budgetArray) ? $budgetArray->string[0] : $budgetArray[0];
+			$budgetArray = !is_array($budgetArray) ? $budgetArray->string : $budgetArray;
+			$budgets[$budgetArray[0]] =  $budgetArray[1];
 		}
 		return $budgets;
 	}
@@ -48,51 +49,51 @@ class BudgetFactory extends FinderFactory
 		]);
 		if ($this->getLogin()->process()) {
 			$service = $this->getService();
-			$budgetRequest = new Request\Catalog\Budget();
-			foreach ($budgetNames as $budgetName) {
-				$budgetRequest->addBudget($budgetName);
-			}
-			$budgetRequest->setDim1From(0);
-			$budgetRequest->setDim1To(9999);
-			$budgetRequest->setOffice($office);
-			$budgetRequest->setPeriodFrom(0);
-			$budgetRequest->setPeriodTo(56);
-			$budgetRequest->setYear($year);
-			$response = $service->send($budgetRequest);
-			$this->setResponse($response);
-			if ($response->isSuccessful()) {
-				$responseDOM = $response->getResponseDocument();
-				$budgets = array();
-				foreach ((new \DOMXPath($responseDOM))->query('//budgets/budget') as $budget) {
-					$budgetArray = [];
-					/**
-					 * @var \DOMElement $budget
-					 */
-					$budgetArray['period'] = $budget->getElementsByTagName('period')->item(0)->textContent;
-					$dim1 = $budget->getElementsByTagName('dim1')->item(0);
-					$budgetArray['dim1'] = [
-						'code' => $dim1->textContent,
-						'name' => $dim1->getAttribute('name'),
-						'type' => $dim1->getAttribute('type'),
-					];
-					$budgetArray['current_balance'] = $budget->getElementsByTagName('current')->item(0)
-						->getElementsByTagName('balance')->item(0)->textContent;
-					$budgetArray['budget_balance'] = $budget->getElementsByTagName('budget')->item(0)
-						->getElementsByTagName('balance')->item(0)->textContent;
-					$groups = [];
-					foreach ($budget->getElementsByTagName('group') as $group) {
-						$groups[] = [
-							'id' => $group->getAttribute('id'),
-							'name' => $group->getAttribute('name'),
-							'code' => $group->textContent,
+			$budgets = array();
+			foreach ($budgetNames as $budgetCode => $budgetName) {
+				$budgetRequest = new Request\Catalog\Budget();
+				$budgetRequest->addBudget($budgetCode);
+				$budgetRequest->setDim1From(0);
+				$budgetRequest->setDim1To(9999);
+				$budgetRequest->setOffice($office);
+				$budgetRequest->setPeriodFrom(0);
+				$budgetRequest->setPeriodTo(56);
+				$budgetRequest->setYear($year);
+				$response = $service->send($budgetRequest);
+				$this->setResponse($response);
+				if ($response->isSuccessful()) {
+					$responseDOM = $response->getResponseDocument();
+					foreach ((new \DOMXPath($responseDOM))->query('//budgets/budget') as $budget) {
+						$budgetArray = [];
+						/**
+						 * @var \DOMElement $budget
+						 */
+						$budgetArray['period'] = $budget->getElementsByTagName('period')->item(0)->textContent;
+						$dim1 = $budget->getElementsByTagName('dim1')->item(0);
+						$budgetArray['dim1'] = [
+							'code' => $dim1->textContent,
+							'name' => $dim1->getAttribute('name'),
+							'type' => $dim1->getAttribute('type'),
 						];
+						$budgetArray['current_balance'] = $budget->getElementsByTagName('current')->item(0)
+							->getElementsByTagName('balance')->item(0)->textContent;
+						$budgetArray['budget_balance'] = $budget->getElementsByTagName('budget')->item(0)
+							->getElementsByTagName('balance')->item(0)->textContent;
+						$groups = [];
+						foreach ($budget->getElementsByTagName('group') as $group) {
+							$groups[] = [
+								'id' => $group->getAttribute('id'),
+								'name' => $group->getAttribute('name'),
+								'code' => $group->textContent,
+							];
+						}
+						$budgetArray['groups'] = $groups;
+						$budgetArray['name'] = $budgetName;
+						$budgets[] = $budgetArray;
 					}
-					$budgetArray['groups'] = $groups;
-					$budgets[] = $budgetArray;
 				}
-
-				return $budgets;
 			}
+			return $budgets;
 		}
 	}
 }
